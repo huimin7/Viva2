7f2920c56c1b9d8cb5dcc3e5bf3b5db936233194
huimin7 <147672948+huimin7@users.noreply.github.com>
2023-12-10 19:22:52 +0800

    Add files via upload

---
+++ Q3.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package viva2;
+
+/**
+ *
+ * @author Chuimin
+ */
+import java.util.Scanner;
+
+public class Q3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+        
+        String str=sc.nextLine();
+        String strValue="";
+        
+        for(int i=0;i<str.length()-1;i++){
+            strValue += ""+str.charAt(i);
+        }
+       // System.out.println(strValue);
+        String [] numValue=strValue.split(",");
+        
+        
+        boolean [] validity =validity(numValue);
+        
+        System.out.print("{");
+        for(int i=0;i<6;i++){
+            if(validity[i]){
+                System.out.printf("True");
+                if(i==5){
+                    break;
+                }
+                System.out.print(", ");
+            }else{
+                System.out.print("False");
+                if(i==5){
+                    break;   
+                }
+                System.out.print(", ");
+            }
+        }
+        System.out.print("}");
+        
+    
+              
+    }    
+        
+    
+    
+     public static boolean [] validity(String [] arr){
+        boolean [] valid=new boolean[6];
+        for(int i=0;i<6;i++){
+            String a="";
+            for(int j=2;j<arr[i].length()-1;j++){
+                a += "" + arr[i].charAt(j);
+            }
+           // System.out.print(a);
+            if(isNumber(a)){
+               valid[i]=true; 
+            }
+            
+        }
+        return valid;
+    }
+    
+    
+    public static boolean isNumber(String element){
+        
+        try{
+            Double.parseDouble(element);
+            return true;
+        }catch(NumberFormatException e){
+            return false;
+        }
+    }
+    
+}
